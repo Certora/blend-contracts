e48cf850252f90f1fc0174ce01d29ea4e2f3a5ce
Chandrakana Nandi <chandra.nandi90@gmail.com>
2024-11-20 15:39:04 -0800

    move files around

--- pool/src/certora_specs/spec.rs
+++ pool/src/certora_specs/spec.rs
@@ -8,8 +8,8 @@ use crate::{pool, PoolContract};
 #[rule]
 pub fn user_health(env: &Env, user: Address) {
     let positions = <PoolContract as crate::Pool>::get_positions(env.clone(), user);
-    let mut pool_state = pool::Pool::load(&env);
-    let data: pool::PositionData = pool::PositionData::calculate_from_positions(&env, &mut pool_state, &positions);
+    // let mut pool_state = pool::Pool::load(&env);
+    // let data: pool::PositionData = pool::PositionData::calculate_from_positions(&env, &mut pool_state, &positions);
     // assert!(data.as_health_factor() > data.scalar);
     assert!(false);
 }
