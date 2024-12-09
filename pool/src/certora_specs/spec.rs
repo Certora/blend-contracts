a2515da4a58dae5231771e6b5f5fe64e9152e439
Chandrakana Nandi <cnandi@cs.washington.edu>
2024-12-08 16:06:43 -0800

    Update spec.rs

--- pool/src/certora_specs/spec.rs
+++ pool/src/certora_specs/spec.rs
@@ -8,8 +8,8 @@ use crate::{pool, Pool, PoolContract};
 #[rule]
 pub fn user_health(env: &Env, user: Address) {
     let positions = PoolContract::get_positions(env.clone(), user);
-    // let mut pool_state = pool::Pool::load(&env);
-    // let data: pool::PositionData = pool::PositionData::calculate_from_positions(&env, &mut pool_state, &positions);
-    // assert!(data.as_health_factor() > data.scalar);
-    assert!(false);
+    let mut pool_state = pool::Pool::load(&env);
+    let data: pool::PositionData = pool::PositionData::calculate_from_positions(&env, &mut pool_state, &positions);
+    assert!(data.as_health_factor() > data.scalar);
+    // assert!(false);
 }
