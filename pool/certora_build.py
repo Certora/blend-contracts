57af7e1be7d64cbe3ca2628610c892a0ccc3c8b5
Chandrakana Nandi <chandra.nandi90@gmail.com>
2024-12-11 17:34:05 -0800

    changes from arie

--- pool/certora_build.py
+++ pool/certora_build.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import argparse
 import json
 import subprocess
@@ -15,9 +16,15 @@ PROJECT_DIR = (SCRIPT_DIR / "../").resolve()
 SOURCES = ["pool/src/pool/*.rs", "backstop/src/*.rs", "Cargo.toml"]
 EXECUTABLES = "target/wasm32-unknown-unknown/release/pool.wasm"
 
+VERBOSE = False
+
+def log(msg):
+    if VERBOSE:
+        print(msg, file=sys.stderr)
+
 def run_command(command, to_stdout=False):
     """Runs the build command and dumps output to temporary files."""
-    print(f"Running {command}")
+    log(f"Running '{command}'")
     try:
         if to_stdout:
             result = subprocess.run(
@@ -27,7 +34,8 @@ def run_command(command, to_stdout=False):
             )
             return None, None, result.returncode
         else:
-            with tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.stdout.log') as stdout_file, tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.stderr.log') as stderr_file:
+            with tempfile.NamedTemporaryFile(delete=False, mode='w', prefix="certora_build_", suffix='.stdout') as stdout_file, \
+                tempfile.NamedTemporaryFile(delete=False, mode='w', prefix="certora_build_", suffix='.stderr') as stderr_file:
                 # Compile rust project and redirect stdout and stderr to a temp file
                 result = subprocess.run(
                     command,
@@ -38,7 +46,7 @@ def run_command(command, to_stdout=False):
                 )
                 return stdout_file.name, stderr_file.name, result.returncode
     except Exception as e:
-        print(f"Error running command '{command}': {e}")
+        log(f"Error running command '{command}': {e}")
         return None, None -1
 
 def write_output(output_data, output_file=None):
@@ -46,32 +54,37 @@ def write_output(output_data, output_file=None):
     if output_file:
         with open(output_file, 'w') as f:
             json.dump(output_data, f, indent=4)
-        print(f"Output written to {output_file}")
+        log(f"Output written to {output_file}")
     else:
-        print(json.dumps(output_data, indent=4))
+        print(json.dumps(output_data, indent=4), file=sys.stdout)
 
 def main():
     parser = argparse.ArgumentParser(description="Compile rust projects and generate JSON output to be used by Certora Prover.")
     parser.add_argument("-o", "--output", metavar="FILE", help="Path to output JSON to a file.")
-    parser.add_argument("-j", "--json", action="store_true", help="Dump JSON output to the console.")
+    parser.add_argument("--json", action="store_true", help="Dump JSON output to the console.")
     parser.add_argument("-l", "--log", action="store_true", help="Show log outputs from cargo build on standard out.")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Be verbose.")
 
     args = parser.parse_args()
+    global VERBOSE
+    VERBOSE = args.verbose
+
     to_stdout = args.log
     
     # Compile rust project and dump the logs to tmp files
     stdout_log, stderr_log, return_code = run_command(COMMAND, to_stdout)
     
-    print(f"Temporary log file located at: {stdout_log} and {stderr_log}")
+    if stdout_log is not None:
+        log(f"Temporary log file located at:\n\t{stdout_log}\nand\n\t{stderr_log}")
     
     # JSON template
     output_data = {
         "project_directory": str(PROJECT_DIR),
         "sources": SOURCES,
         "executables": EXECUTABLES,
-        "success": return_code,
-        "stdout_log": stdout_log,
-        "stderr_log": stderr_log
+        "success": True if return_code == 0 else False,
+        "return_code": return_code,
+        "log" : {"stdout": stdout_log, "stderr": stderr_log}
     }
     
     # Handle output based on the provided argument
@@ -82,7 +95,7 @@ def main():
         write_output(output_data)
         
     # Needed for mutations: if you run _this_ script inside another script, you can check this returncode and decide what to do
-    sys.exit(0 if return_code else 1)
+    sys.exit(0 if return_code == 0 else 1)
 
 if __name__ == "__main__":
     main()
